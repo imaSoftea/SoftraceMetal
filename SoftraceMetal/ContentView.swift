c494c94c768b8558e75928e9558d45d1bd35fd8c
Carter Thompson <94035527+imaSoftea@users.noreply.github.com>
2023-03-02 20:02:22 -0500

    Initial Commit

---
+++ SoftraceMetal/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  SoftraceMetal
+//
+//  Created by Carter Thompson on 3/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
